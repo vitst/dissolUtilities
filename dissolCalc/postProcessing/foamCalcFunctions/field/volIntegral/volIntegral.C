6e0bde7331e65261b57cf3de06fcc5292c97b5c3
Vitaliy Starchenko <vitaliy.starchenko@gmail.com>
2016-12-14 16:52:26 -0500

    Added volume integral to the field section

---
+++ dissolCalc/postProcessing/foamCalcFunctions/field/volIntegral/volIntegral.C
@@ -0,0 +1,118 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "volIntegral.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    namespace calcTypes
+    {
+        defineTypeNameAndDebug(volIntegral, 0);
+        addToRunTimeSelectionTable(calcType, volIntegral, dictionary);
+    }
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::calcTypes::volIntegral::volIntegral()
+:
+    calcType()
+{}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::calcTypes::volIntegral::~volIntegral()
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void Foam::calcTypes::volIntegral::init()
+{
+    argList::validArgs.append("volIntegral");
+    argList::validArgs.append("fieldName");
+}
+
+
+void Foam::calcTypes::volIntegral::preCalc
+(
+    const argList& args,
+    const Time& runTime,
+    const fvMesh& mesh
+)
+{}
+
+
+void Foam::calcTypes::volIntegral::calc
+(
+    const argList& args,
+    const Time& runTime,
+    const fvMesh& mesh
+)
+{
+    const word fieldName = args[2];
+
+    IOobject fieldHeader
+    (
+        fieldName,
+        runTime.timeName(),
+        mesh,
+        IOobject::MUST_READ
+    );
+
+    // Check field exists
+    if (fieldHeader.headerOk())
+    {
+        bool processed = false;
+
+        writeVolIntegralField<scalar>(fieldHeader, mesh, processed);
+        writeVolIntegralField<vector>(fieldHeader, mesh, processed);
+        writeVolIntegralField<sphericalTensor>(fieldHeader, mesh, processed);
+        writeVolIntegralField<symmTensor>(fieldHeader, mesh, processed);
+        writeVolIntegralField<tensor>(fieldHeader, mesh, processed);
+
+        if (!processed)
+        {
+            FatalError
+                << "Unable to process " << fieldName << nl
+                << "No call to volIntegral for fields of type "
+                << fieldHeader.headerClassName() << nl << nl
+                << exit(FatalError);
+        }
+    }
+    else
+    {
+        Info<< "    No " << fieldName << endl;
+    }
+}
+
+
+// ************************************************************************* //
+
